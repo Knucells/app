9345c01db1f9f8a2eb8e389220b3a39ded6ebd60
Mhairi Parr <m.s.parr.09@aberdeen.ac.uk>
2015-04-10 02:46:04 +0000

    Added results to bar + Test changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
---
+++ test/controllers/nokogiri_controller_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class NokogiriControllerTest < ActionController::TestCase
+end
